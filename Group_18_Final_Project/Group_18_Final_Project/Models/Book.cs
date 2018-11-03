b7de60eba0ea4e58984af01081f701a3778d74ac
kevin-langley <43178084+kevin-langley@users.noreply.github.com>
2018-11-03 17:58:38 -0500

    Added Model Classes
    Added book class and genre class

---
+++ Group_18_Final_Project/Group_18_Final_Project/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group_18_Final_Project.Models
+{
+    public class Book
+    {
+        public Int32 BookID { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public Int32 UniqueID { get; set; }
+        public Int32 TimesPurched { get; set; }
+        public Int32 AverageRating { get; set; }
+        public Int32 CopiesOnHand { get; set; }
+        public Decimal BookPrice { get; set; }
+        public Decimal WholesalePrice { get; set; }
+        public Boolean Active { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public BookOrder BookOrder { get; set; }
+        public Review Review { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
