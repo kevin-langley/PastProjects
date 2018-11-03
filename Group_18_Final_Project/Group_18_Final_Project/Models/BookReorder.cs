81e914216083dba5602820bb204fee79ef58d3af
hoopdreams <mitchcantway@hotmail.com>
2018-11-03 18:11:03 -0500

    Update Group_18_Final_Project/Group_18_Final_Project/Models/BookReorder.cs

---
+++ Group_18_Final_Project/Group_18_Final_Project/Models/BookReorder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group_18_Final_Project.Models
+{
+    public class BookReorder
+    {
+        public Int32 BookReorderID { get; set; }
+        public String Quantity { get; set; }
+        public Book Book { get; set; }
+        public Reorder Reorder { get; set; }
+
+    }
+}
