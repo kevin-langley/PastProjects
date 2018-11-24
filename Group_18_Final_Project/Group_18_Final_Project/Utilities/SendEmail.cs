c8d598d495ab995aa8244c199a8c1496b1aedd78
kevin-langley <43178084+kevin-langley@users.noreply.github.com>
2018-11-24 11:43:28 -0600

    Began Recreating email utility

---
+++ Group_18_Final_Project/Group_18_Final_Project/Utilities/SendEmail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace Group_18_Final_Project.Utilities
+{
+    public class SendEmail
+    {
+        public IActionResult Contact(string toaddress, string recipient, string subject, string body)
+        {
+            ViewData["Message"] = "Your Contact page TODO.";
+            //instantiate mimemessage
+            var message = new MimeMessage;
+            //from address
+            message.From.Add(new MailboxAddress("Admin", "fa18group18@gmail.com"));
+            //to address
+
+            //subject
+
+            //body
+
+
+            return View();
+        }
+    }
+}
