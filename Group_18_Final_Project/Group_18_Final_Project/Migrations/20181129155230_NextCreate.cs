636c753e93ebba328287e6fc388bccdb4da77ec8
Zoë <zoetruong@utexas.edu>
2018-11-29 09:53:26 -0600

    Migration update

---
+++ Group_18_Final_Project/Group_18_Final_Project/Migrations/20181129155230_NextCreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Group_18_Final_Project.Migrations
+{
+    public partial class NextCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPending",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPending",
+                table: "Orders");
+        }
+    }
+}
