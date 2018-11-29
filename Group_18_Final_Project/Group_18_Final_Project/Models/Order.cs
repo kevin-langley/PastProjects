d0fe5da3073764387c604de7b0c57d5575bb836d
Zoë <zoetruong@utexas.edu>
2018-11-29 10:45:07 -0600

    Updated Order controller and Order model

--- Group_18_Final_Project/Group_18_Final_Project/Models/Order.cs
+++ Group_18_Final_Project/Group_18_Final_Project/Models/Order.cs
@@ -16,7 +16,7 @@ namespace Group_18_Final_Project.Models
         public CreditCard CreditCard { get; set; }
         public User User { get; set; }
         public Coupon Coupon { get; set; }
-        public List<BookOrder> Bookorders { get; set; }
+        public List<BookOrder> BookOrders { get; set; }
 
     }
 }
