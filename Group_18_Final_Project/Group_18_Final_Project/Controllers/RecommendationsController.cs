f859c89eb21522d7778a4b3839b6f502bbf62ca3
angeliawu <42920321+angeliawu@users.noreply.github.com>
2018-11-29 10:46:09 -0600

    Create RecommendationsController.cs

---
+++ Group_18_Final_Project/Group_18_Final_Project/Controllers/RecommendationsController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group_18_Final_Project.Controllers
+{
+    public class RecommendationsController : Controller
+    {
+    }
+}
